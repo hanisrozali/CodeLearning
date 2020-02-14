bddf4337b50830237988d20bd6c1a4b81bfdc1a8
hanis <sitinurhanishh@gmail.com>
2020-02-14 21:23:07 +1000

    for loop

---
+++ ForLoop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	//for loop must have
+	//initialization
+	//comparison
+	//update
+	
+	int i;
+
+	for (i = 5; i <=10; i++)
+	
+	{
+
+		printf("%d\n", i);
+
+	}
+
+	return 0;
+}
